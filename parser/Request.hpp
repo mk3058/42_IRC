57ea3b08011219b45d4a3d21ced319c992cd8607
mk3058 <kmk3058@icloud.com>
2024-01-20 19:10:52 +0900

    feat(Parser): add Request parser class

---
+++ parser/Request.hpp
@@ -0,0 +1,21 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include "Command.hpp"
+#include "Parameter.hpp"
+#include "Prefix.hpp"
+
+class Request {
+ private:
+  Prefix pf;
+  Command cmd;
+  Parameter param;
+
+ public:
+  Request(const std::string &message);
+  Prefix prefix() const;
+  Command command() const;
+  Parameter parameter() const;
+};
+
+#endif
