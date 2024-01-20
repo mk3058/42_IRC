9cfaabe9202787a619eee8befcfc8b99254068b9
mk3058 <kmk3058@icloud.com>
2024-01-20 19:31:18 +0900

    feat(Request): add message validation function

--- request/Request.cpp
+++ request/Request.cpp
@@ -1,12 +1,24 @@
 #include "Request.hpp"
 
+#include <exception>
+
 static const std::string messageDelimeter = " ";
+static const std::string messageEndingKeyword = "\r\n";
 enum { PREFIX, COMMAND, PARAMETER };
+
+bool isValidFormat(std::string message) {
+  // 메시지가 ("\r\n")으로 끝나지 않는 경우에 대한 예외처리
+  if (message.find(messageEndingKeyword) !=
+      message.length() - messageEndingKeyword.length()) {
+    return false;
+  }
+  return true;
+}
 
 void parseRequest(std::string message, std::string (&token)[3]) {
   size_t pos = 0;
 
-  //prefix 파트가 존재하면 해당 부분 저장 후 삭제
+  // prefix 파트가 존재하면 해당 부분 저장 후 삭제
   if (message.at(0) == ':') {
     pos = message.find(messageDelimeter);
     token[PREFIX] = message.substr(1, pos);
@@ -14,7 +26,7 @@ void parseRequest(std::string message, std::string (&token)[3]) {
   } else {
     token[PREFIX] = "";
   }
-  
+
   pos = message.find(messageDelimeter);
   token[COMMAND] = message.substr(0, pos);
   message.erase(0, pos + messageDelimeter.length());
@@ -26,6 +38,10 @@ void parseRequest(std::string message, std::string (&token)[3]) {
  * Parameter : 클라이언트로부터 받은 메시지 전문
  */
 Request::Request(const std::string &message) {
+  if (!isValidFormat(message)) {
+    throw std::invalid_argument("요청 메시지 형식이 잘못되었습니다.");
+  }
+
   std::string token[3];
 
   parseRequest(message, token);
