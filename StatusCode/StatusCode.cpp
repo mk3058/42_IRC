ea1e18c6f53f0586e168c5120360e58b21a882a0
임종민 <imjongmin@imjongmin-ui-MacBookAir.local>
2024-01-20 20:17:49 +0900

    feat(upgrade): camel and comment

--- StatusCode/StatusCode.cpp
+++ StatusCode/StatusCode.cpp
@@ -2,22 +2,25 @@
 
 
 std::map<std::string, std::string> IrcStatusCodeHelper::statusMap;
+bool IrcStatusCodeHelper::init = false;
 
-void IrcStatusCodeHelper::InitialMap()
+void IrcStatusCodeHelper::initialMap()
 {
-    statusMap["RPL_WELCOME"] = "001";
-    statusMap["RPL_BOUNCE"] = "005";
-    statusMap["RPL_AWAY"] = "301";
-    statusMap["RPL_CHANNELMODEIS"] = "324";
-    statusMap["RPL_UNIQOPIS"] = "325";
-    statusMap["RPL_NOTOPIC"] = "331";
-    statusMap["RPL_TOPIC"] = "332";
-    statusMap["RPL_INVITING"] = "341";
-    statusMap["RPL_INVITELIST"] = "346";
-    statusMap["RPL_ENDOFINVITELIST"] = "347";
+    statusMap["RPL_WELCOME"] = "001"; // connect is success
+    statusMap["RPL_BOUNCE"] = "005"; // server is already fully
+    statusMap["RPL_AWAY"] = "301"; // When an invited user is temporarily away (INVITE)
+    statusMap["RPL_CHANNELMODEIS"] = "324"; // if user join channel, informs the current channel mode (MODE, JOIN)
+    statusMap["RPL_UNIQOPIS"] = "325"; // Returned when the user is an operator upon inquiry. (MODE +o)
+    statusMap["RPL_NOTOPIC"] = "331"; // When a user queries the topic, and the topic has not been set. (TOPIC)
+    statusMap["RPL_TOPIC"] = "332"; // if topic is set and user join channel, send a topic massage (JOIN, TOPIC)
     // statusMap["RPL_BANLIST"] = "367"; // Banned rule list (MODE)
     // statusMap["RPL_ENDOFBANLIST"] = "368"; // after code 367, to signal completion of the inquiry. (MODE)
-    statusMap["ERR_NOSUCHNICK"] = "401";
+    statusMap["RPL_INVITING"] = "341"; // when invited user is successfully in channel. (INVITE)
+    statusMap["RPL_INVITELIST"] = "346"; // List of users with invite privileges (MODE +i)
+    statusMap["RPL_ENDOFINVITELIST"] = "347"; // after code 346, to signal completion of the inquiry (MODE +i)
+    // statusMap["RPL_EXCEPTLIST"] = "348"; // when exception list is set and user check the list. (MODE +e)
+    // statusMap["RPL_ENDOFEXCEPTLIST"] = "349"; // after code 348, to signal completion of the inquiry (MODE +e)
+    statusMap["ERR_NOSUCHNICK"] = "401"; // nickname or channel no such in server (INVITE)
     // statusMap["ERR_NOSUCHCHANNEL"] = "403"; // no such channel (JOIN, PART, KICK)
     // statusMap["ERR_TOOMANYCHANNELS"] = "405"; // already joined too many channed (JOIN)
     // statusMap["ERR_TOOMANYTARGETS"] = "407"; // too many targets (JOIN)
@@ -35,11 +38,14 @@ void IrcStatusCodeHelper::InitialMap()
     statusMap["ERR_BADCHANMASK"] = "476"; // (channel name incorrect syntax or pattern, length limit) (JOIN, KICK)
     statusMap["ERR_NOCHANMODES"] = "477"; // Channel doesn't support modes (MODE, TOPIC)
     statusMap["ERR_CHANOPRIVSNEEDED"] = "482"; // not channel's operator (MODE, TOPIC, INVITE, KICK)
+    init = true;
 }
 
 
-std::string& IrcStatusCodeHelper::GetStatusCode(const std::string& statusName)
+std::string& IrcStatusCodeHelper::getStatusCode(const std::string& statusName)
 {
+    if (init == false)
+        throw std::invalid_argument("statusMap is not initializied");
     const std::map<std::string, std::string>::iterator it = statusMap.find(statusName);
     if (it != statusMap.end()){
         return it->second;
