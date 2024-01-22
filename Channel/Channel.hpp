b0527d41b683db72a0f11e29a6aaa9cbfd29f5ad
mk3058 <kmk3058@icloud.com>
2024-01-22 21:47:55 +0900

    feat(Channel): add Channel class

---
+++ Channel/Channel.hpp
@@ -0,0 +1,40 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <map>
+
+#include "UserMap.hpp"
+
+#define UNLIMITED -1
+#define DEFAULT 0b1
+#define INVITE_ONLY 0b10
+#define USERMODE_TOPIC 0b10
+#define USERMODE_SUPER 0b110
+
+class Channel {
+private:
+  std::string name;
+  std::string topic;
+  std::string password;
+  int userLimit;
+  int mode;
+  UserMap users;
+  UserMap banned_user;
+  std::map<int, int> userPermit;
+ public:
+  Channel(std::string name, User user);
+  void setTopic(std::string topic);
+  void setMode(int mode);
+  void setUserLimit(int limit);
+  void kickUser(User user);
+  const std::string& getName() const;
+  const std::string& getTopic() const;
+  const std::string& getPassword() const;
+  int getUserLimit() const;
+  int getMode() const;
+  const UserMap& getUsers() const;
+  const UserMap& getBannedUsers() const;
+  const std::map<int, int>& getUserPermits() const;
+};
+
+#endif
