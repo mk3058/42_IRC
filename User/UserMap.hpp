010d3a71576e7388a55ae2a2d9496ec6f312ac00
mk3058 <kmk3058@icloud.com>
2024-01-22 21:48:22 +0900

    feat(User): add User class

---
+++ User/UserMap.hpp
@@ -0,0 +1,19 @@
+#ifndef USERMAP_HPP
+#define USERMAP_HPP
+
+#include <map>
+#include "User.hpp"
+
+class UserMap {
+private:
+    std::map<int, User> userMap;
+
+public:
+    UserMap();
+    ~UserMap();
+    void addUser(int fd, User user);
+    void deleteUser(int fd);
+    User findUser(int fd) const;
+};
+
+#endif
