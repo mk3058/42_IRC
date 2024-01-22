cefdf3eb54740d6acf8f59bdee3b8ea310c4daa9
mk3058 <kmk3058@icloud.com>
2024-01-22 16:52:16 +0900

    feat(Response): add response message builder

---
+++ response/Response.hpp
@@ -0,0 +1,20 @@
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+#define DEFAULT_PREFIX "webserv.com"
+
+#include <string>
+#include <vector>
+
+class Response {
+ public:
+  static std::string build(std::string responseCode, std::string prefix,
+                           std::vector<std::string> params,
+                           std::string trailer);
+  static std::string build(std::string responseCode, std::string prefix,
+                           std::vector<std::string> params);
+  static std::string build(std::string responseCode, std::string prefix);
+  static std::string build(std::string responseCode);
+};
+
+#endif
