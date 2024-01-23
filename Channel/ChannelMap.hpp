6e0ed36fa6c021d5f13bccc4545c98a98708db71
mk3058 <kmk3058@icloud.com>
2024-01-23 17:41:04 +0900

    feat(ChannelMap): add ChannelMap class

---
+++ Channel/ChannelMap.hpp
@@ -0,0 +1,20 @@
+#ifndef CHANNELMAP_HPP
+#define CHANNELMAP_HPP
+
+#include <map>
+
+#include "Channel.hpp"
+
+class ChannelMap {
+ private:
+  std::map<std::string, Channel> channelMap;
+
+ public:
+  ChannelMap();
+  ~ChannelMap();
+  void addChannel(Channel channel);
+  void deleteChannel(std::string channelName);
+  const Channel &findChannel(std::string channelName);
+};
+
+#endif
