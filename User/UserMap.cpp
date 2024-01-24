4a9cb5e2fc4c69d43b1315ff6e255ab3236a20d6
mk3058 <kmk3058@icloud.com>
2024-01-24 13:11:01 +0900

    feat: add find methods

--- User/UserMap.cpp
+++ User/UserMap.cpp
@@ -7,17 +7,24 @@ UserMap::UserMap() {}
 UserMap::~UserMap() {}
 
 void UserMap::addUser(int fd, User user) {
-  std::pair<std::map<int, User>::iterator, bool> result;
+  std::pair<std::map<int, User>::iterator, bool> fdMapResult;
+  std::pair<std::map<std::string, User>::iterator, bool> nicknameMapResult;
 
-  result = userMap.insert(std::make_pair(fd, user));
-  if (!result.second) {
+  fdMapResult = userMap.insert(std::make_pair(fd, user));
+  nicknameMapResult =
+      nicknameMap.insert(std::make_pair(user.getNickname(), user));
+  if (!fdMapResult.second || !nicknameMapResult.second) {
     throw std::invalid_argument("Cannot add user!! duplicate key exist");
   }
 }
 
 void UserMap::deleteUser(int fd) {
-  std::map<int, User>::iterator result = userMap.find(fd);
-  userMap.erase(result);
+  std::map<int, User>::iterator fdMapResult = userMap.find(fd);
+  std::map<std::string, User>::iterator nicknameMapResult =
+      nicknameMap.find(fdMapResult->second.getNickname());
+
+  userMap.erase(fdMapResult);
+  nicknameMap.erase(nicknameMapResult);
 }
 
 User UserMap::findUser(int fd) const {
@@ -29,8 +36,25 @@ User UserMap::findUser(int fd) const {
   return result->second;
 }
 
+User UserMap::findUser(std::string nickname) const {
+  std::map<std::string, User>::const_iterator result =
+      nicknameMap.find(nickname);
+
+  if (result == nicknameMap.end()) {
+    throw std::invalid_argument("cannot find user");
+  }
+  return result->second;
+}
+
 bool UserMap::exists(int fd) {
   std::map<int, User>::const_iterator result = userMap.find(fd);
 
   return result != userMap.end();
+}
+
+bool UserMap::exists(std::string nickname) {
+  std::map<std::string, User>::const_iterator result =
+      nicknameMap.find(nickname);
+
+  return result != nicknameMap.end();
 }
--- User/UserMap.hpp
+++ User/UserMap.hpp
@@ -2,19 +2,24 @@
 #define USERMAP_HPP
 
 #include <map>
+#include <string>
+
 #include "User.hpp"
 
 class UserMap {
-private:
-    std::map<int, User> userMap;
+ private:
+  std::map<int, User> userMap;
+  std::map<std::string, User> nicknameMap;
 
-public:
-    UserMap();
-    ~UserMap();
-    void addUser(int fd, User user);
-    void deleteUser(int fd);
-    User findUser(int fd) const;
-    bool exists(int fd);
+ public:
+  UserMap();
+  ~UserMap();
+  void addUser(int fd, User user);
+  void deleteUser(int fd);
+  User findUser(int fd) const;
+  User findUser(std::string nickname) const;
+  bool exists(int fd);
+  bool exists(std::string nickname);
 };
 
 #endif
