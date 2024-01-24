ce48576ba51d9532baa2d3e120ba292557a82dec
임종민 <imjongmin@imjongmin-ui-MacBookAir.local>
2024-01-24 18:54:00 +0900

    fix(Server): add var 'totalUsers'

---
+++ Controller/Certification.cpp
@@ -0,0 +1,26 @@
+#include "Certification.hpp"
+
+Certification::Certification(Request request, User *user, fd_set *fd_write)
+ : ICommand(request, user, fd_write) {}
+
+
+void    Certification::excute()
+{
+    if ()
+    Server::getInstance().
+}
+
+bool            Certification::checkPermit()
+{
+
+}
+
+std::string     Certification::getResponse() const
+{
+
+}
+
+int             Certification::getCount() const
+{
+
+}
