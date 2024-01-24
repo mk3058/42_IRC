ed73659111a8f95adc91b73ec32729a171b7a44c
mk3058 <kmk3058@icloud.com>
2024-01-24 15:02:24 +0900

    feat(ICommand): add constructors of ICommand class

--- Controller/ICommand.hpp
+++ Controller/ICommand.hpp
@@ -2,25 +2,28 @@
 #define ICOMMAND_HPP
 
 #include <iostream>
-#include "Channel.hpp"
-#include "User.hpp"
-#include "Request.hpp"
 
-class ICommand
-{
-    protected:
-        Request req;
-        User    user;
-        Channel channel;
-        int permission;
-        int write_cnt;
-        fd_set *fd_write;
-        std::string msg;
-        virtual void execute() = 0;
-        virtual bool checkPermit();
-    public:
-        virtual std::string getResponse() const;
-        virtual int getCount() const;
+#include "Channel.hpp"
+#include "Request.hpp"
+#include "User.hpp"
+
+class ICommand {
+ protected:
+  Request req;
+  User user;
+  Channel channel;
+  int permission;
+  int write_cnt;
+  fd_set *fd_write;
+  std::string msg;
+  virtual void execute() = 0;
+  virtual bool checkPermit();
+
+ public:
+  ICommand();
+  ICommand(Request request, User user, Channel channel, fd_set *fd_write);
+  virtual std::string getResponse() const;
+  virtual int getCount() const;
 };
 
 #endif
