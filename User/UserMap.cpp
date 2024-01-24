7051ccdef37421a4567ea3c8424c9d2673646056
mk3058 <kmk3058@icloud.com>
2024-01-24 14:39:48 +0900

    Merge branch 'master' into feat/#26

--- User/UserMap.cpp
+++ User/UserMap.cpp
@@ -36,6 +36,37 @@ User UserMap::findUser(int fd) const {
   return result->second;
 }
 
-int UserMap::getSize() const{
-  return this->userMap.size();
+int UserMap::getSize() const { return this->userMap.size(); }
+
+User UserMap::findUser(std::string nickname) const {
+  std::map<std::string, User>::const_iterator result =
+      nicknameMap.find(nickname);
+
+  if (result == nicknameMap.end()) {
+    throw std::invalid_argument("cannot find user");
+  }
+  return result->second;
+}
+
+bool UserMap::exists(int fd) {
+  std::map<int, User>::const_iterator result = userMap.find(fd);
+
+  return result != userMap.end();
+}
+
+bool UserMap::exists(std::string nickname) {
+  std::map<std::string, User>::const_iterator result =
+      nicknameMap.find(nickname);
+
+  return result != nicknameMap.end();
+}
+
+std::vector<User> UserMap::findAllUsers() const {
+  std::vector<User> users;
+
+  for (std::map<int, User>::const_iterator it = userMap.begin();
+       it != userMap.end(); ++it) {
+    users.push_back(it->second);
+  }
+  return users;
 }
--- User/UserMap.hpp
+++ User/UserMap.hpp
@@ -12,13 +12,17 @@ class UserMap {
   std::map<int, User> userMap;
   std::map<std::string, User> nicknameMap;
 
-public:
-    UserMap();
-    ~UserMap();
-    int getSize() const;
-    void addUser(int fd, User user);
-    void deleteUser(int fd);
-    User findUser(int fd) const;
+ public:
+  UserMap();
+  ~UserMap();
+  int getSize() const;
+  void addUser(int fd, User user);
+  void deleteUser(int fd);
+  User findUser(int fd) const;
+  User findUser(std::string nickname) const;
+  bool exists(int fd);
+  bool exists(std::string nickname);
+  std::vector<User> findAllUsers() const;
 };
 
 #endif
