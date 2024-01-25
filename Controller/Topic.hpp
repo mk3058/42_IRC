fefd422dc25625407c25608085004fd1df9ad252
mk3058 <kmk3058@icloud.com>
2024-01-25 17:51:12 +0900

    feat(Topic): add Topic class

---
+++ Controller/Topic.hpp
@@ -0,0 +1,25 @@
+#ifndef TOPIC_HPP
+#define TOPIC_HPP
+
+#include <vector>
+
+#include "ICommand.hpp"
+#include "Server.hpp"
+#include "StatusCode.hpp"
+
+class Topic : public ICommand {
+ private:
+  bool validate();
+  void changeTopic();
+  void showTopic();
+  static Server &server;
+  static ChannelMap &serverChannels;
+  static UserMap &serverUsers;
+
+ public:
+  Topic(Request request, User *user);
+  virtual void execute();
+  virtual bool checkPermit();
+};
+
+#endif
