9fb65be0f175688c784568fe1240fe927fa5e041
mk3058 <kmk3058@icloud.com>
2024-01-25 13:45:38 +0900

    feat(ICommand): change fd_write variable from a pointer to a regular variable

--- Controller/ICommand.cpp
+++ Controller/ICommand.cpp
@@ -1,9 +1,11 @@
 #include "ICommand.hpp"
 
-ICommand::ICommand() : user(NULL), fd_write(NULL) {}
+ICommand::ICommand() : user(NULL) { FD_ZERO(&fd_write); }
 
-ICommand::ICommand(Request request, User *user, fd_set *fd_write)
-    : req(request), user(user), fd_write(fd_write) {}
+ICommand::ICommand(Request request, User *user)
+    : req(request), user(user) {
+  FD_ZERO(&fd_write);
+}
 
 bool ICommand::checkPermit() {
   if (permission == USERMODE_SUPER) return (true);
