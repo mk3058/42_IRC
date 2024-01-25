277a30b5f38e490d474ec5f316ce4411ce3f3261
임종민 <imjongmin@imjongmin-ui-MacBookAir.local>
2024-01-25 16:45:40 +0900

    fix(controller): controller fix

--- Controller/Controler.cpp
+++ Controller/Controler.cpp
@@ -1,74 +1,92 @@
 #include "Controler.hpp"
 
-Controler::Controler(Request &request, fd_set &fd_write)
+Controler::Controler(Request &requeste, User *user)
 {
     this->request = request;
-    this->fd_write = fd_write;
     msgLength = 0;
     fdCnt = 0;
 }
 
 void    Controler::execute()
 {
-    // if (request.command().getCommand() == "CERTIFICATION")
-    // {
-    //     Certification certi(request);
-    //     responseMessage = certi.getResponse();
-    //     fdCnt = certi.getCnt();
-    // }
-    // else if (request.command().getCommand() == "JOIN")
-    // {
-    //     Join join(request);
-    //     responseMessage = join.getResponse();
-    //     fdCnt = join.getCnt();
-    // }
-    // else if (request.command().getCommand() == "PART")
-    // {
-    //     Part part(request);
-    //     responseMessage = part.getResponse();
-    //     fdCnt = part.getCnt();
-    // }
-    // else if (request.command().getCommand() == "KICK")
-    // {
-    //     Kick kick(request);
-    //     responseMessage = kick.getResponse();
-    //     fdCnt = kick.getCnt();
-    // }
-    // else if (request.command().getCommand() == "INVITE")
-    // {
-    //     Invite invite(request);
-    //     responseMessage = invite.getResponse();
-    //     fdCnt = invite.getCnt();
-    // }
-    // else if (request.command().getCommand() == "TOPIC")
-    // {
-    //     Topic topic(request);
-    //     responseMessage = topic.getResponse();
-    //     fdCnt = topic.getCnt();
-    // }
-    // else if (request.command().getCommand() == "MODE")
-    // {
-    //     Mode mode(request);
-    //     responseMessage = mode.getResponse();
-    //     fdCnt = mode.getCnt();
-    // }
-    // else if (request.command().getCommand() == "DCC")
-    // {
-    //     Dcc dcc(request);
-    //     responseMessage = dcc.getResponse();
-    //     fdCnt = dcc.getCnt();
-    // }
-    // else if (request.command().getCommand() == "BOT")
-    // {
-    //     Bot bot(request);
-    //     responseMessage = bot.getResponse();
-    //     fdCnt = bot.getCnt();
-    // }
-    // else
-    // {
-    //     responseMessage = "Not found command";
-    //     fdCnt = 1;
-    // }
+    std::string cmd = request.command().getCommand();
+    if (Server::getInstance().)
+    {
+    if (cmd == "PASS")
+    {
+        Certification certi(request);
+        responseMessage = certi.getResponse();
+        fdCnt = certi.getCnt();
+    }
+    else if (cmd == "NICK")
+    {
+        
+        Certification certi(request);
+        responseMessage = certi.getResponse();
+        fdCnt = certi.getCnt();
+    }
+    
+    else if (cmd == "USER")
+    {
+        Certification certi(request);
+        responseMessage = certi.getResponse();
+        fdCnt = certi.getCnt();
+    }
+    }
+    else if (cmd == "JOIN")
+    {
+        Join join(request);
+        responseMessage = join.getResponse();
+        fdCnt = join.getCnt();
+    }
+    else if (cmd == "PART")
+    {
+        Part part(request);
+        responseMessage = part.getResponse();
+        fdCnt = part.getCnt();
+    }
+    else if (cmd == "KICK")
+    {
+        Kick kick(request);
+        responseMessage = kick.getResponse();
+        fdCnt = kick.getCnt();
+    }
+    else if (cmd == "INVITE")
+    {
+        Invite invite(request);
+        responseMessage = invite.getResponse();
+        fdCnt = invite.getCnt();
+    }
+    else if (cmd == "TOPIC")
+    {
+        Topic topic(request);
+        responseMessage = topic.getResponse();
+        fdCnt = topic.getCnt();
+    }
+    else if (cmd == "MODE")
+    {
+        Mode mode(request);
+        responseMessage = mode.getResponse();
+        fdCnt = mode.getCnt();
+    }
+    else if (cmd == "DCC")
+    {
+        Dcc dcc(request);
+        responseMessage = dcc.getResponse();
+        fdCnt = dcc.getCnt();
+    }
+    else if (cmd == "BOT")
+    {
+        Bot bot(request);
+        responseMessage = bot.getResponse();
+        fdCnt = bot.getCnt();
+    }
+    else
+    {
+        responseMessage = "Not found command";
+        fdCnt = 1;
+    }
+    }
 
     msgLength = responseMessage.size();
 }
