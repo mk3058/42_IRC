ce3932787d9f99dc632bdc89e7526ceb3df4379f
mk3058 <kmk3058@icloud.com>
2024-01-29 17:02:21 +0900

    fix(Pass): fixed an issue that caused a segmentation fault

--- Controller/Pass.hpp
+++ Controller/Pass.hpp
@@ -3,18 +3,15 @@
 
 #include "ICommand.hpp"
 
+class Pass : public ICommand {
+ private:
+ public:
+  Pass(Request request, User *user);
 
-class Pass : public ICommand
-{
-    private:
-
-    public:
-        Pass(Request request, User *user);
-
-        void execute();
-        bool checkPermit();
-        std::string getResponse() const;
-        int getCount() const;
+  void execute();
+  bool checkPermit();
+  std::string getResponse() const;
+  int getCount() const;
 };
 
 #endif
