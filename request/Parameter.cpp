74cbf81370a59503b3deb0a3e2e898a24c2646c5
mk3058 <kmk3058@icloud.com>
2024-02-01 14:51:25 +0900

    fix: 파라미터 파싱에 빈 문자열이 포함되지 않도록 수정

--- request/Parameter.cpp
+++ request/Parameter.cpp
@@ -1,7 +1,7 @@
 #include "Parameter.hpp"
 
 static const std::string parameterDelimeter = " ";
-static const std::string trailerDelimeter = " :";
+static const std::string trailerDelimeter = ":";
 
 static void parseParameter(std::string parameter,
                            std::vector<std::string> &token, std::string &tr,
@@ -11,7 +11,8 @@ static void parseParameter(std::string parameter,
 
   // trailer가 존재할 경우 해당 값을 별도로 저장하고 parameter 문자열에서 삭제
   size_t pos;
-  if ((pos = parameter.find(trailerDelimeter)) != std::string::npos) {
+  if ((pos = parameter.find(trailerDelimeter)) != std::string::npos &&
+      !parameter.substr(pos + trailerDelimeter.length()).empty()) {
     tr = parameter.substr(pos + trailerDelimeter.length());
     TrailerExists = true;
     parameter.erase(pos);
@@ -20,7 +21,7 @@ static void parseParameter(std::string parameter,
   // 구분자(" ")를 기준으로 파라미터를 tokenize 하여 주어진 벡터에 저장
   while (true) {
     pos = parameter.find(parameterDelimeter);
-    if (pos) {  // 빈 문자열이 아닐때만 추가
+    if (!parameter.substr(0, pos).empty()) {  // 빈 문자열이 아닐때만 추가
       token.push_back(parameter.substr(0, pos));
     }
     if (pos == std::string::npos) {
@@ -42,6 +43,9 @@ Parameter::Parameter() : TrailerExists(false) {}
  * Parameter : 파라미터 문자열. 파라미터가 존재하지 않을 경우 빈 문자열
  */
 void Parameter::set(const std::string &parameter) {
+  if (parameter.empty()) {
+    return;
+  }
   parseParameter(parameter, params, trailer, TrailerExists);
 }
 
