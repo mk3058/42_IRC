567d21aaca875b90ab6338e6d6015b075536c086
mk3058 <kmk3058@icloud.com>
2024-02-02 14:29:39 +0900

    fix(Kick): 응답 메시지 형식 수정

--- Controller/Kick.cpp
+++ Controller/Kick.cpp
@@ -3,9 +3,9 @@
 Kick::Kick(Request req, User *user) : ICommand(req, user) {
   param = true;
   this->channelMap = &Server::getInstance().getChannelMap();
-  if (!req.parameter().getParameters().size()) param = false;
-  else if (channelMap->exists(req.parameter().getParameters()[0].substr(1)))
-  {
+  if (req.parameter().getParameters().size() < 2)
+    param = false;
+  else if (channelMap->exists(req.parameter().getParameters()[0].substr(1))) {
     this->channel = &(
         channelMap->findChannel(req.parameter().getParameters()[0].substr(1)));
     this->permission = channel->getMode();
@@ -19,10 +19,12 @@ void Kick::execute() {
   User *targetUser = &channel->getUsers().findUser(userName);
   targetUser->leaveChannel(channel->getName());
   //  내보내졌다는 메시지 보내기
-  msg = Response::build(
-      req.command().getCommand(), req.parameter().getParameters(),
-      "user " + targetUser->getNickname() + " Kick this channel",
-      user->getNickname() + "!" + user->getUsername() + "@" DEFAULT_PREFIX);
+  std::string trailer = req.parameter().isTrailerExists()
+                            ? req.parameter().getTrailer()
+                            : "No reason";
+  msg = Response::build(req.command().getCommand(),
+                        req.parameter().getParameters(), trailer,
+                        user->getNickname() + "!" + user->getUsername());
   write_cnt = channel->getUsers().getSize();
   for (int i = 0; i < write_cnt; ++i)
     FD_SET(channel->getUsers().findAllUsers()[i]->getfd(), &fd_write);
@@ -36,40 +38,39 @@ void Kick::execute() {
 }
 
 bool Kick::checkPermit() {
+  std::vector<std::string> params;
   int fd = user->getfd();
   FD_SET(fd, &fd_write);
-  // 채널명이 잘못됨
-  if (param == false)
-  {
-    msg = Response::error(ERR_NEEDMOREPARAMS, *user, &fd_write, "parameter error(kick)");
+
+  // 파라미터 부족
+  if (param == false) {
+    msg = Response::build(ERR_NEEDMOREPARAMS, params, "More Parameter Needed!");
     Server::getInstance().bufferMessage(msg, 1, &fd_write);
     return (false);
   }
+  // 채널이 없음
   if (!channelMap->exists(req.parameter().getParameters()[0].substr(1))) {
-    msg = Response::error(ERR_NOSUCHCHANNEL, *user, &fd_write,
-                          "wrong channel name");
+    params.push_back(user->getNickname());
+    params.push_back(req.parameter().getParameters()[0].substr(1));
+    msg = Response::build(ERR_NOSUCHCHANNEL, params, "No such channel!");
     Server::getInstance().bufferMessage(msg, 1, &fd_write);
     return (false);
   }
   // 권한확인이 먼저
   int upermission = channel->getUserPermits()[user->getfd()];
   if (!(upermission & USERMODE_SUPER)) {
-    msg = Response::error(ERR_CHANOPRIVSNEEDED, *user, &fd_write,
-                          "you are not operator");
-    Server::getInstance().bufferMessage(msg, 1, &fd_write);
-    return (false);
-  }
-  // 유저네임이 안들어옴
-  if (req.parameter().getParameters().size() < 2) {
-    msg =
-        Response::error(ERR_NEEDMOREPARAMS, *user, &fd_write, "need user name");
+    params.push_back(user->getNickname());
+    params.push_back("#" + channel->getName());
+    msg = Response::build(ERR_CHANOPRIVSNEEDED, params,
+                          "You're not channel operator!");
     Server::getInstance().bufferMessage(msg, 1, &fd_write);
     return (false);
   }
   // 유저네임이 잘못됨
   if (!channel->getUsers().exists(req.parameter().getParameters()[1])) {
-    msg = Response::error(ERR_USERNOTINCHANNEL, *user, &fd_write,
-                          "user not in channel");
+    params.push_back(user->getNickname());
+    params.push_back(req.parameter().getParameters()[1]);
+    msg = Response::build(ERR_NOSUCHNICK, params, "No such nick/channel!");
     Server::getInstance().bufferMessage(msg, 1, &fd_write);
     return (false);
   }
